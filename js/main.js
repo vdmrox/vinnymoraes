5f2e93aabaf7bbbce65b1bb20bec479ab30f4463
vdmrox <vdmrox@gmail.com>
2016-11-01 14:33:00 -0200

    updating images and transitions

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 $('body').vegas({
-    delay: 7000,
+    delay: 10000,
+    preloadImage:true,
     timer: true,
     shuffle: true,    
-    transition: 'blur',
-    transitionDuration: 1000,
+    transition: ['flash'],
+    transitionDuration: 3000,
     slides: [
-        { src: '/images/1.jpg' },
-        { src: '/images/3.jpg' },
-        { src: '/images/4.jpg' },
-        { src: '/images/5.jpg' }
+        { src: '/images/img01.jpg' },
+        { src: '/images/img02.jpg' },
+        { src: '/images/img03.jpg' },
+        { src: '/images/img04.jpg' },
+        { src: '/images/img05.jpg' },
+        { src: '/images/img06.jpg' },
+        { src: '/images/img07.jpg' },
+        { src: '/images/img08.jpg' },
+        { src: '/images/img09.jpg' },
+        { src: '/images/img10.jpg' }
     ],
-    overlay: '/images/overlays/01.png'
+    overlay: '/images/overlays/01.png',
+    animation: 'kenburns',
+    animationDuration: 11000
 });
